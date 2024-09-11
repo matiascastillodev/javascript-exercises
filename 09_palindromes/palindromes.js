c7a6d3462fd54827de15208c5d86ff97d3499f43
matiascastilloaudio <177944762+matiascastilloaudio@users.noreply.github.com>
2024-09-11 10:23:26 -0300

    submit exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    if (typeof str == "string") {
+        let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '')
+        let revStr = cleanStr.split('').reverse().join('');
+        if (cleanStr == revStr) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return "Error. Not a String";
+    }
 };
 
 // Do not edit below this line
