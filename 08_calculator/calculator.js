8d854f996b51d866538d16919882601e5ca682db
matiascastilloaudio <177944762+matiascastilloaudio@users.noreply.github.com>
2024-09-11 00:39:09 -0300

    submit exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...numbers]) {
+  return numbers.reduce((acc, current) => acc + (current || 0), 0);
 };
 
-const multiply = function() {
-
+const multiply = function ([...numbers]) {
+  return numbers.reduce((acc, current) => acc * (current || 1), 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let output = 1;
+  for (let i = a; i >= 1; i--) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
